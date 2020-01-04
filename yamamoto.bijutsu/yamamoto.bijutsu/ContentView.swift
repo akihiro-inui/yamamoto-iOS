812c52dfecb943100466bd585ef780ebe111befb
inuinana <mail@akihiroinui.com>
2020-01-04 20:50:10 +0100

    Initial commit

---
+++ yamamoto.bijutsu/yamamoto.bijutsu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yamamoto.bijutsu
+//
+//  Created by Akihiro Inui on 04/01/2020.
+//  Copyright © 2020 Akihiro Inui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
